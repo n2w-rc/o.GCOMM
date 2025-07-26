0539bfa292a1a57e72e43f33db87a2a154343a94
n2w-rc <127403802+n2w-rc@users.noreply.github.com>
2025-07-26 12:23:58 +0800

    Attempted re-design with whatever came to mind.

--- react-o1/src/DashboardContent.js
+++ react-o1/src/DashboardContent.js
@@ -5,13 +5,13 @@ function DashboardContent() {
     <main className="container my-4">
       <div className="row">
         <div className="col-md-8">
-          <div className="card">
+          <div className="card fade-in">
             <div className="card-body">
               <h2 className="card-title">Main Content</h2>
               <p className="card-text">This is the main content area. You can add your components and content here.</p>
             </div>
           </div>
-          <div className="card mt-4">
+          <div className="card mt-4 slide-up">
             <div className="card-body">
               <h2 className="card-title">Recent Posts</h2>
               <p className="card-text">Facebook posts will be displayed here.</p>
@@ -19,7 +19,7 @@ function DashboardContent() {
           </div>
         </div>
         <div className="col-md-4">
-          <div className="card">
+          <div className="card fade-in">
             <div className="card-body">
               <h3 className="card-title">Sidebar</h3>
               <p className="card-text">This is the sidebar. You can add navigation or other content here.</p>
